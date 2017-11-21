4fedc5f27c7687ac5076fc9855f8ca9b34f58021
Palma <palma@item.ntnu.no>
2017-11-21 11:57:55 +0100

    Support for disabling DTLS in CoAP proxy connections

--- proxy/http_coap/src/proxy.c
+++ proxy/http_coap/src/proxy.c
@@ -36,7 +36,9 @@
 #include <signal.h>
 #include <getopt.h>
 #include <unistd.h>
+#ifdef COAP_DTLS_EN
 #include <gnutls/gnutls.h>
+#endif
 #include "listener.h"
 #include "connection.h"
 #include "param.h"
@@ -94,7 +96,9 @@ int main(int argc, char **argv)
     struct sigaction sai = {{0}};
     const char *config_file_name = CONFIG_FILE_NAME;
     const char *short_opts = ":hc:";
+#ifdef COAP_DTLS_EN
     const char *gnutls_ver = NULL;
+#endif
     tls_server_t server = {0};
     listener_t *listener = NULL;
     unsigned listener_index = 0;
@@ -174,6 +178,7 @@ int main(int argc, char **argv)
 
     coap_log_set_level(param_get_max_log_level(&param));
 
+#ifdef COAP_DTLS_EN
     gnutls_ver = gnutls_check_version(NULL);
     if (gnutls_ver == NULL)
     {
@@ -182,6 +187,7 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
     coap_log_info("GnuTLS version: %s", gnutls_ver);
+#endif
 
     /* initialise SSL/TLS */
     ret = tls_init();
